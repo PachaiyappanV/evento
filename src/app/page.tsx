a7b5f38febc41a893096d9f83b33745a95570428
PachaiyappanV <pachaiyappan394@gmail.com>
2024-08-19 15:23:44 +0530

    feat(tailwind): add accent color #a4f839

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,14 @@ export default function Home() {
       </h1>
       <p className="mt-7 mb-12 text-2xl lg:text-3xl opacity-75">
         Browse more than{" "}
-        <span className="italic underline text-[#a4f839] font-bold">
-          10,000
-        </span>{" "}
+        <span className="italic underline text-accent font-bold">10,000</span>{" "}
         events around you
       </p>
       <form className=" w-full sm:w-[580px]">
         <input
           type="text"
           className="w-full h-16 rounded-lg outline-none bg-white/[7%] px-6 
-          ring-[#a4f839] focus:ring-2 focus:bg-white/10"
+          ring-accent focus:ring-2 focus:bg-white/10"
           placeholder="Search events in any city..."
           spellCheck={false}
         />
