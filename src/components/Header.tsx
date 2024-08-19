adf3807fc587a536e554070da6f61176e7252466
PachaiyappanV <pachaiyappan394@gmail.com>
2024-08-19 17:07:43 +0530

    feat(ui): add active link slider using Framer Motion

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,7 @@
+"use client";
+import { motion } from "framer-motion";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import Logo from "./Logo";
 
 const routes = [
@@ -12,17 +15,29 @@ const routes = [
   },
 ];
 const Header = () => {
+  const activePathName = usePathname();
   return (
     <header
       className="flex items-center justify-between h-14 border-b 
     border-white/20 px-3 sm:px-9 "
     >
       <Logo />
-      <nav>
-        <ul className="flex gap-x-6 text-sm">
+      <nav className=" h-full">
+        <ul className="flex gap-x-6 text-sm h-full">
           {routes.map((route) => (
-            <li key={route.name} className="text-white/50 hover:text-white">
+            <li
+              key={route.name}
+              className={`${
+                activePathName === route.path ? "text-white" : "text-white/50"
+              } hover:text-white flex items-center relative`}
+            >
               <Link href={route.path}>{route.name}</Link>
+              {activePathName === route.path && (
+                <motion.div
+                  layoutId="header-active-link"
+                  className="h-1 w-full bg-accent absolute bottom-0"
+                ></motion.div>
+              )}
             </li>
           ))}
         </ul>
