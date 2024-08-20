17ecc31f7e9c192737a65e02ed15fc613f8bea48
PachaiyappanV <pachaiyappan394@gmail.com>
2024-08-20 18:44:28 +0530

    feat: add fetching events for the correct city

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -7,19 +7,19 @@ type Props = {
     city: string;
   };
 };
-const EventsPage = async ({ params }: Props) => {
+const EventsPage = async ({ params: { city } }: Props) => {
   const response = await fetch(
-    "https://bytegrad.com/course-assets/projects/evento/api/events?city=austin"
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
   );
   const events: EventoEvent[] = await response.json();
 
   return (
     <main className="flex flex-col items-center py-24 px-4 md:px-6 min-h-[110vh]">
       <H1>
-        {params.city === "all" && "All Events"}
-        {params.city !== "all" && (
+        {city === "all" && "All Events"}
+        {city !== "all" && (
           <span>
-            Events in <span className="capitalize">{params.city}</span>
+            Events in <span className="capitalize">{city}</span>
           </span>
         )}
       </H1>
